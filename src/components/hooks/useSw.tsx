072af27eca1431710b49c66700ea58c55987dfe2
T3RM1N4L <108430379+T3M1N4L@users.noreply.github.com>
2024-07-05 20:15:22 -0500

    oops silly mistake

--- src/components/hooks/useSw.tsx
+++ src/components/hooks/useSw.tsx
@@ -5,7 +5,7 @@ const useSw = (path: string) => {
   useEffect(() => {
     if ("serviceWorker" in navigator) {
       navigator.serviceWorker.ready.then(() => {
-        SetTransport("EpxMod.libcurlClient", { wisp: "wss://nebulaproxy.io/wisp/"});
+        SetTransport("CurlMod.libcurlClient", { wisp: "wss://nebulaproxy.io/wisp/"});
       })
       navigator.serviceWorker
       .register(path)
