5a09e297cf03f49dd62070e552f6cafe66305363
T3RM1N4L <108430379+T3M1N4L@users.noreply.github.com>
2024-07-22 15:16:04 -0500

    wokr wokr wokr

--- src/components/hooks/useSw.tsx
+++ src/components/hooks/useSw.tsx
@@ -7,12 +7,13 @@ app.use("/libcurl/", express.static(libcurlPath));
 app.use("/baremux/", express.static(baremuxPath));
 import { BareMuxConnection } from "@mercuryworkshop/bare-mux"
 let connection = new BareMuxConnection("/baremux/worker.js")
+await connection.setTransport("/libcurl/index.mjs", [{ wisp: "wss://wisp.mercurywork.shop/" }]);
 
 const useSw = (path: string) => {
   useEffect(() => {
     if ("serviceWorker" in navigator) {
       navigator.serviceWorker.ready.then(() => {
-        await connection.setTransport("/libcurl/index.mjs", [{ wisp: "wss://wisp.mercurywork.shop/" }]);
+        
       })
       navigator.serviceWorker
       .register(path)
