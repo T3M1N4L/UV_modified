cd1d62952cd8e2e13d6a6bd2238b62ceb109f7d2
T3RM1N4L <108430379+T3M1N4L@users.noreply.github.com>
2024-07-22 15:07:33 -0500

    uh

--- src/components/hooks/useSw.tsx
+++ src/components/hooks/useSw.tsx
@@ -1,11 +1,18 @@
 import { useEffect } from "react";
 import SetTransport from "@mercuryworkshop/bare-mux";
+import { baremuxPath } from "@mercuryworkshop/bare-mux/node";
+import { libcurlPath } from "@mercuryworkshop/libcurl-transport";
+
+app.use("/libcurl/", express.static(libcurlPath));
+app.use("/baremux/", express.static(baremuxPath));
+import { BareMuxConnection } from "@mercuryworkshop/bare-mux"
+let connection = new BareMuxConnection("/baremux/worker.js")
 
 const useSw = (path: string) => {
   useEffect(() => {
     if ("serviceWorker" in navigator) {
       navigator.serviceWorker.ready.then(() => {
-        SetTransport("CurlMod.LibcurlClient", { wisp: "wss://nebulaproxy.io/wisp/" });
+        await connection.setTransport("/libcurl/index.mjs", [{ wisp: "wss://wisp.mercurywork.shop/" }]);
       })
       navigator.serviceWorker
       .register(path)
