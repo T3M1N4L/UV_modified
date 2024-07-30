5a0299b118b03d2b47b20cfc2f9e2b0dce318cbd
T3RM1N4L <108430379+T3M1N4L@users.noreply.github.com>
2024-07-29 22:54:50 -0500

    shawlg

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -132,10 +132,10 @@ const Navbar = () => {
       <div className="container flex h-14 max-w-screen-2xl items-center">
         <div className="mr-4 hidden md:flex">
           <a className="mr-6 flex items-center space-x-2" href={`/`}>
-            <section className="h-6 w-6" id="gallery">
-            <img className="h-6 w-6" class="blured-bg" src="/iceberg.png">
-            <img className="h-6 w-6" class="item" src="/iceberg.png">
-            </section>
+            <div className="h-6 w-6" id="gallery">
+            <img className="h-6 w-6 blured-bg" src="/iceberg.png">
+            <img className="h-6 w-6 item" src="/iceberg.png">
+            </div>
             <span className="text-card-foreground"><strong>UV_modified</strong></span>
           </a>
           <nav className="flex items-center gap-6 text-sm">
